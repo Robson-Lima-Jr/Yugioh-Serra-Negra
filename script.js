7fa5776c79769183595ffd4e140d371a16e8eaa8
Robson-Jr <robsonjuniorsn@gmail.com>
2023-04-11 18:12:15 -0300

    update-1.0, metade atualizado

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function btnClick() {
     let fehDecks = document.querySelector("aside#feh-decks");
 
     let playerList = document.getElementsByName("players");
-    let areaDeck = document.querySelector("article#deckArea");
 
     if (playerList[0].checked) {
         jaoDecks.style.display = "block";
@@ -16,7 +15,9 @@ function btnClick() {
         viDecks.style.display = "none";
         fahDecks.style.display = "none";
         fehDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     } else if (playerList[1].checked) {
         tilaDecks.style.display = "block";
         jaoDecks.style.display = "none";
@@ -24,7 +25,9 @@ function btnClick() {
         viDecks.style.display = "none";
         fahDecks.style.display = "none";
         fehDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     } else if (playerList[2].checked) {
         robDecks.style.display = "block";
         jaoDecks.style.display = "none";
@@ -32,7 +35,9 @@ function btnClick() {
         viDecks.style.display = "none";
         fahDecks.style.display = "none";
         fehDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     } else if (playerList[3].checked) {
         viDecks.style.display = "block";
         jaoDecks.style.display = "none";
@@ -40,7 +45,9 @@ function btnClick() {
         robDecks.style.display = "none";
         fahDecks.style.display = "none";
         fehDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     } else if (playerList[4].checked) {
         fahDecks.style.display = "block";
         jaoDecks.style.display = "none";
@@ -48,7 +55,9 @@ function btnClick() {
         robDecks.style.display = "none";
         viDecks.style.display = "none";
         fehDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     } else if (playerList[5].checked) {
         fehDecks.style.display = "block";
         jaoDecks.style.display = "none";
@@ -56,14 +65,139 @@ function btnClick() {
         robDecks.style.display = "none";
         viDecks.style.display = "none";
         fahDecks.style.display = "none";
-        areaDeck.style.display = "none";
+        jaoDeck.style.display = "none";
+        tilaDeck.style.display = "none";
+        robDeck.style.display = "none";
     }
 }
+//Area de cliques nas imagens para abrir os arquivos
+//jao
+let jaoDeck = document.querySelector("article#player1");
+let rei = document.querySelector("section#rei-deck");
+let dino = document.querySelector("section#dino-deck");
+let gouki = document.querySelector("section#gouki-deck");
+let abc = document.querySelector("section#abc-deck");
+let pescador = document.querySelector("section#pescador-deck");
+//tila
+let tilaDeck = document.querySelector("article#player2");
+let buster = document.querySelector("section#buster-deck");
+let blue = document.querySelector("section#blue-deck");
+let dark = document.querySelector("section#dark-deck");
+//rob
+let robDeck = document.querySelector("article#player3");
+let cyber = document.querySelector("section#cyber-deck");
+let cyberdark = document.querySelector("section#cyberdark-deck");
+let uria = document.querySelector("section#uria-deck");
+let dinomist = document.querySelector("section#dinomist-deck");
+let malefic = document.querySelector("section#malefic-deck");
 
 function dino_click() {
-    let areaDeck = document.querySelector("article#deckArea");
-    let imgClick = document.querySelector("section#dino-deck");
+    jaoDeck.style.display = "block";
+    dino.style.display = "block";
+    gouki.style.display = "none";
+    abc.style.display = "none";
+    rei.style.display = "none";
+    pescador.style.display = "none";
+}
 
-    areaDeck.style.display = "block";
-    imgClick.style.display = "block";
+function gouki_click() {
+    jaoDeck.style.display = "block";
+    gouki.style.display = "block";
+    dino.style.display = "none";
+    abc.style.display = "none";
+    rei.style.display = "none";
+    pescador.style.display = "none";
+}
+
+function abc_click() {
+    jaoDeck.style.display = "block";
+    abc.style.display = "block";
+    dino.style.display = "none";
+    gouki.style.display = "none";
+    rei.style.display = "none";
+    pescador.style.display = "none";
+}
+
+function rei_click() {
+    jaoDeck.style.display = "block";
+    rei.style.display = "block";
+    dino.style.display = "none";
+    gouki.style.display = "none";
+    abc.style.display = "none";
+    pescador.style.display = "none";
+}
+
+function pescador_click() {
+    jaoDeck.style.display = "block";
+    pescador.style.display = "block";
+    dino.style.display = "none";
+    gouki.style.display = "none";
+    abc.style.display = "none";
+    rei.style.display = "none";
+}
+
+function buster_click() {
+    tilaDeck.style.display = "block";
+    buster.style.display = "block";
+    blue.style.display = "none";
+    dark.style.display = "none";
+}
+
+function blue_click() {
+    tilaDeck.style.display = "block";
+    blue.style.display = "block";
+    buster.style.display = "none";
+    dark.style.display = "none";
+}
+
+function dark_click() {
+    tilaDeck.style.display = "block";
+    dark.style.display = "block";
+    buster.style.display = "none";
+    blue.style.display = "none";
+}
+
+function cyber_click() {
+    robDeck.style.display = "block";
+    cyber.style.display = "block";
+    cyberdark.style.display = "none";
+    uria.style.display = "none";
+    dinomist.style.display = "none";
+    malefic.style.display = "none";
+}
+
+function cyberdark_click() {
+    robDeck.style.display = "block";
+    cyberdark.style.display = "block";
+    cyber.style.display = "none";
+    uria.style.display = "none";
+    dinomist.style.display = "none";
+    malefic.style.display = "none";
+}
+
+function uria_click() {
+    robDeck.style.display = "block";
+    uria.style.display = "block";
+    cyber.style.display = "none";
+    cyberdark.style.display = "none";
+    dinomist.style.display = "none";
+    malefic.style.display = "none";
+}
+
+function dinomist_click() {
+    robDeck.style.display = "block";
+    dinomist.style.display = "block";
+    cyber.style.display = "none";
+    cyberdark.style.display = "none";
+    uria.style.display = "none";
+    malefic.style.display = "none";
+}
+
+function malefic_click() {
+    robDeck.style.display = "block";
+    malefic.style.display = "block";
+    cyber.style.display = "none";
+    cyberdark.style.display = "none";
+    uria.style.display = "none";
+    dinomist.style.display = "none";
 }
